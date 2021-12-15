006a2987f3cf38a48c3ee8a7d36990a705e55719
airspaced-nk5 <75466142+airspaced-nk5@users.noreply.github.com>
2021-12-14 23:29:08 -0500

    More package

--- docs/make.jl
+++ docs/make.jl
@@ -16,6 +16,7 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+           repo = "github.com/airspaced-nk5/GeometricalOptics.jl.git",
+           devbranch = "main"
+       )
